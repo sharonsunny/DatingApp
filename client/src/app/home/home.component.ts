2c5a948ad94d97390af3202a0ac83b6bd5e140ce
sharonsunny <sharonsunnyalice@gmail.com>
2023-02-24 19:55:12 +0530

    End of section 5

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+registerMode=false;
+users:any;
+constructor(private http:HttpClient){
+
+}
+ngOnInit():void{
+  this.getUsers();
+}
+cancelRegisterMode(event:boolean){
+this.registerMode=event;
+}
+registerToggle(){
+  this.registerMode=!this.registerMode;
+}
+getUsers(){
+  this.http.get('https://localhost:5001/api/Users').subscribe({
+    next:response=>this.users=response,
+    error:error=>console.log(error),
+    complete:()=>console.log('Request has completed')
+   })
+}
+}
